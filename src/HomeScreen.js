64cb21664cbbe14c2c8a3cbb2ba73aeecc9d526d
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2023-02-12 01:07:17 +0530

    Update HomeScreen.js

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,10 +6,6 @@ function HomeScreen() {
   return (
     <div className='homeScreen'>
     <Nav/>
-     <img 
-        className='na'
-        src="https://www.whats-on-netflix.com/wp-content/uploads/2022/10/79-new-movies-and-tv-shows-added-to-netflix-uk-this-week-jpeg.webp" alt='logo'/>
-
        
 <h1>Rishav</h1>
 <h1>test</h1>
