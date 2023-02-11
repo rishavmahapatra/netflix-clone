cd5110e0d5af765f5a0ad86b6eea4511ed136a4f
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2023-02-11 23:54:22 +0530

    Update HomeScreen.js

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,6 +6,17 @@ function HomeScreen() {
   return (
     <div className='homeScreen'>
     <Nav/>
+     <div className="nav_contents">
+        <img 
+        className='nav_logo'
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt='logo'/>
+        
+
+        <img 
+        className='nav_avatar'
+        src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/e70b1333850498.56ba69ac32ae3.png' alt='avatar'/>
+
+        </div>
 <h1>test</h1>
 <h1>test</h1>
 <h1>test</h1>
