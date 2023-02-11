b338b53bb1e724b6ba8de3886cb1e35808d3394b
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2023-02-12 00:54:59 +0530

    Update HomeScreen.js

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,10 +6,6 @@ function HomeScreen() {
   return (
     <div className='homeScreen'>
     <Nav/>
-        <img 
-        className='na'
-        src="https://cdn.mos.cms.futurecdn.net/rDJegQJaCyGaYysj2g5XWY-1920-80.jpg.webp" alt='logo'/>
-
        
 <h1>Rishav</h1>
 <h1>test</h1>
